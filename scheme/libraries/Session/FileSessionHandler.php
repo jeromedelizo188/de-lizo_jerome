60018815538fef85fb0792aebc46d0a4ade1f97b
jeromedelizo188 <jeromedelizo9@gmail.com>
2025-10-08 23:11:37 +0800

    Final: Session mkdir fix deployed and controller logic updated

--- scheme/libraries/Session/FileSessionHandler.php
+++ scheme/libraries/Session/FileSessionHandler.php
@@ -81,7 +81,7 @@ class FileSessionHandler extends Session implements SessionHandlerInterface {
         $this->save_path = $save_path;
         $this->file_path = $this->save_path.DIRECTORY_SEPARATOR.$session_name . '_';
          if ( !is_dir($this->save_path) ) {
-            mkdir($this->save_path, 0700, TRUE);
+            @mkdir($this->save_path, 0700, TRUE);
             }
      return true;
     }
